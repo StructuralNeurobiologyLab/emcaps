23d161eb295b0a4eb91894f571c01287022460dd
mdraw <martin.drawitsch@gmail.com>
2022-04-26 00:13:48 +0200

    data v6: per-image splitting, refactor util funcs

---
+++ utils/splitdataset.py
@@ -0,0 +1,123 @@
+"""Image-based dataset splitting into train + validation parts."""
+
+import logging
+import os
+from os.path import expanduser as eu
+from pathlib import Path
+from typing import Tuple, List
+
+import imageio
+import numpy as np
+import pandas as pd
+import skimage
+from scipy import ndimage
+from skimage import measure
+from skimage import morphology as sm
+
+from utils import get_meta, get_path_prefix, get_image_resources
+
+
+# Load original image sheet
+
+path_prefix = get_path_prefix()
+data_root = path_prefix / 'Single-table_database'
+# Image based split
+isplit_data_root = data_root / 'isplitdata_v6'
+sheet_path = data_root / 'Image_annotation_for_ML_single_table.xlsx'
+isplit_data_root.mkdir(exist_ok=True)
+
+meta = get_meta(sheet_path=sheet_path, v5names=True)
+
+
+# Set up logging
+logger = logging.getLogger('splitdataset')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(f'{isplit_data_root}/splitdataset.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+
+def count_ccs(lab):
+    """Count connected components in label image"""
+    _, ncc = measure.label(lab, return_num=True)
+    return ncc
+
+def get_best_split_slices(lab: np.ndarray, valid_ratio: float = 0.3) -> Tuple[Tuple[slice, slice], Tuple[slice, slice]]:
+    """Determine best slices for splitting the image by considering particle ratios"""
+    sh = np.array(lab.shape)
+    vert_border, horiz_border = np.round(sh * valid_ratio).astype(np.int64)
+    vert_slices = (
+        (slice(0, vert_border), slice(0, None)),
+        (slice(vert_border, None), slice(0, None)),
+    )
+    hori_slices = (
+        (slice(0, None), slice(0, horiz_border)),
+        (slice(0, None), slice(horiz_border, None)),
+    )
+
+    # Count number of particles in each subimage
+    val_vert_nc = count_ccs(lab[vert_slices[0]])
+    trn_vert_nc = count_ccs(lab[vert_slices[1]])
+    val_hori_nc = count_ccs(lab[hori_slices[0]])
+    trn_hori_nc = count_ccs(lab[hori_slices[1]])
+    vert_split_nc_ratio = val_vert_nc / (val_vert_nc + trn_vert_nc)
+    hori_split_nc_ratio = val_hori_nc / (val_hori_nc + trn_hori_nc)
+    logger.info(f'vsr: {vert_split_nc_ratio:.2f}, hsr: {hori_split_nc_ratio:.2f}')
+
+    # Choose the split that minimizes difference between particle ratio and split ratio
+    vert_split_penalty = abs(vert_split_nc_ratio - valid_ratio)
+    hori_split_penalty = abs(hori_split_nc_ratio - valid_ratio)
+    if vert_split_penalty <= hori_split_penalty:
+        best_slices = vert_slices
+    else:
+        best_slices = hori_slices
+
+    return best_slices
+
+
+def split_by_slices(img: np.ndarray, slices: Tuple[Tuple[slice, slice], Tuple[slice, slice]]) -> Tuple[np.ndarray, np.ndarray]:
+    val = img[slices[0]]
+    train = img[slices[1]]
+    return val, train
+
+
+def is_excluded(resmeta: pd.Series) -> bool:
+    return (not resmeta.Validation) and (not resmeta.Training)
+
+
+for entry in meta.itertuples():
+    img_num = entry.num
+    # Load original images and resources
+    res = get_image_resources(img_num=img_num, sheet_path=sheet_path, use_curated_if_available=True)
+    if is_excluded(res.metarow):
+        logger.info(f'Skipping image {img_num} because it is excluded from ML usage via meta spreadsheet.')
+        continue
+    if res.label is None:
+        logger.info(f'Skipping image {img_num} because no label was found.')
+        continue
+    if res.raw is None:
+        logger.info(f'Skipping image {img_num} because no raw image was found.')
+        continue
+
+    # Split images
+    split_slices = get_best_split_slices(res.label)
+    val_raw, trn_raw = split_by_slices(res.raw, split_slices)
+    val_lab, trn_lab = split_by_slices(res.label, split_slices)
+
+    # Scale for image viewer compat
+    val_lab = val_lab.astype(np.uint8) * 255
+    trn_lab = trn_lab.astype(np.uint8) * 255
+
+    # Save newly split images
+    img_subdir = isplit_data_root / str(img_num)
+    img_subdir.mkdir(exist_ok=True)
+    val_raw_path = img_subdir / f'{img_num}_val.tif'
+    trn_raw_path = img_subdir / f'{img_num}_trn.tif'
+    val_lab_path = img_subdir / f'{img_num}_val_encapsulins.tif'
+    trn_lab_path = img_subdir / f'{img_num}_trn_encapsulins.tif'
+    imageio.imwrite(val_raw_path, val_raw)
+    imageio.imwrite(trn_raw_path, trn_raw)
+    imageio.imwrite(val_lab_path, val_lab)
+    imageio.imwrite(trn_lab_path, trn_lab)
+
+
--- utils/utils.py
+++ utils/utils.py
@@ -5,8 +5,11 @@ Utility functions and resources.
 # TODO: Eliminate duplicated code elsewhere by importing from here.
 
 import os
+from typing import Optional, Union
 from os.path import expanduser as eu
 from pathlib import Path
+from functools import lru_cache
+from dataclasses import dataclass
 
 import imageio
 import numpy as np
@@ -20,12 +23,13 @@ from skimage import measure
 from skimage import morphology as sm
 
 
+
 def eul(paths):
     """Shortcut for expanding all user paths in a list"""
     return [os.path.expanduser(p) for p in paths]
 
 
-def image_grid(imgs, rows, cols, enable_grid_lines=True, text_color=255):
+def image_grid(imgs, rows, cols, enable_grid_lines=True, text_color=255) -> Image.Image:
     """Draw images sequentially on a (rows * cols) grid."""
     assert len(imgs) == rows*cols
 
@@ -54,58 +58,169 @@ with open(class_info_path) as f:
     class_info = yaml.load(f, Loader=yaml.FullLoader)
 CLASS_IDS = class_info['class_ids_v5']  # Use v5 class names
 CLASS_NAMES = {v: k for k, v in CLASS_IDS.items()}
+LABEL_NAME = class_info['label_name']
 
 OLD_CLASS_IDS = class_info['class_ids']  # Use v5 class names
 OLD_CLASS_NAMES = {v: k for k, v in OLD_CLASS_IDS.items()}
 OLDNAMES_TO_V5NAMES = class_info['_oldnames_to_v5names']
 
 
+def get_path_prefix() -> Path:
+    if os.getenv('CLUSTER') == 'WHOLEBRAIN':
+        path_prefix = Path('/wholebrain/scratch/mdraw/tum/').expanduser()
+    else:
+        path_prefix = Path('~/tum/').expanduser()
+    assert path_prefix.is_dir()
+    return path_prefix
 
-if os.getenv('CLUSTER') == 'WHOLEBRAIN':
-    path_prefix = Path('/wholebrain/scratch/mdraw/tum/').expanduser()
-else:
-    path_prefix = Path('~/tum/').expanduser()
 
-sheet = pd.read_excel(
-    path_prefix / 'Single-table_database/Image_annotation_for_ML_single_table.xlsx',
-    sheet_name='all_metadata'
-)
+@lru_cache(maxsize=1024)
+def get_meta(sheet_path=None, sheet_name=0, v5names=True) -> pd.DataFrame:
+    if sheet_path is None:
+        path_prefix = get_path_prefix()
+        sheet_path = path_prefix / 'Single-table_database/Image_annotation_for_ML_single_table.xlsx'
+        sheet_name = 'all_metadata'
+    sheet = pd.read_excel(sheet_path, sheet_name=sheet_name)
+    meta = sheet.copy()
+    meta = meta.rename(columns={' Image': 'num'})
+    meta = meta.loc[meta['num'] >= 16]
+    meta = meta.rename(columns={'Short experimental condition': 'scond'})
+    meta = meta.convert_dtypes()
+    if v5names:
+        meta.scond.replace(OLDNAMES_TO_V5NAMES, inplace=True)
+    return meta
 
-meta = sheet.copy()
-meta = meta.rename(columns={' Image': 'num'})
-meta = meta.loc[meta['num'] >= 16]
-meta = meta.rename(columns={'Short experimental condition': 'scond'})
-meta = meta.convert_dtypes()
 
 # Use new short names
 OLDNAMES_TO_V5NAMES = class_info['_oldnames_to_v5names']
 # meta.scond.replace(OLDNAMES_TO_V5NAMES, inplace=True)
 
-# Load train/validation split
-valid_split_path = './valid_split.yaml'
-with open(valid_split_path) as f:
-    valid_image_dict = yaml.load(f, Loader=yaml.FullLoader)
+# # Load train/validation split
+# valid_split_path = './valid_split.yaml'
+# with open(valid_split_path) as f:
+#     valid_image_dict = yaml.load(f, Loader=yaml.FullLoader)
 
 
 # Also works for numbers
-def get_meta_row(path):
-    if not isinstance(path, Path):
-        path = Path(str(path))
-    row = meta[meta['num'] == int(path.stem)]
+@lru_cache(maxsize=1024)
+def get_meta_row(path_or_num, *args, **kwargs) -> pd.Series:
+    meta = get_meta(*args, **kwargs)
+    if not isinstance(path_or_num, Path):
+        path = Path(str(path_or_num))
+    row = meta.loc[meta['num'] == int(path.stem)]
+    assert row.shape[0] == 1  # num is unique
+    row = row.squeeze(0) #  -> to pd.Series
     return row
 
-def get_old_enctype(path):
+
+@lru_cache(maxsize=1024)
+def get_old_enctype(path) -> str:
     row = get_meta_row(path)
     old_enctype = row.scond.item()
     assert old_enctype in OLD_CLASS_NAMES.values(), f'{old_enctype} not in {OLD_CLASS_NAMES.values()}'
     return old_enctype
 
-def get_v5_enctype(path):
+
+@lru_cache(maxsize=1024)
+def get_v5_enctype(path) -> str:
     old_enctype = get_old_enctype(path)
     v5_enctype = OLDNAMES_TO_V5NAMES[old_enctype]
     assert v5_enctype in CLASS_NAMES.values(), f'{v5_enctype} not in {CLASS_NAMES.values()}'
     return v5_enctype
 
-def is_for_validation(path):
+
+@lru_cache(maxsize=1024)
+def is_for_validation(path) -> bool:
     row = get_meta_row(path)
     return row.Validation.item()
+
+
+@lru_cache(maxsize=1024)
+def get_raw_path(img_num: int, sheet_path=None) -> Path:
+    if sheet_path is None:
+        path_prefix = get_path_prefix()
+        sheet_path = path_prefix / 'Single-table_database/Image_annotation_for_ML_single_table.xlsx'
+    # meta = get_meta(sheet_path=sheet_path)
+    subdir_path = sheet_path.parent / f'{img_num}'
+    img_path = subdir_path / f'{img_num}.tif'
+    return img_path
+
+@lru_cache(maxsize=1024)
+def get_raw(img_num: int, sheet_path=None) -> np.ndarray:
+    img_path = get_raw_path(img_num=img_num, sheet_path=sheet_path)
+    img = imageio.imread(img_path)
+    return img
+
+
+@lru_cache(maxsize=1024)
+def read_image(path: Path) -> np.ndarray:
+    if not path.is_file():
+        uppercase_suf_path = path.with_suffix('.TIF')
+        if uppercase_suf_path.is_file():
+            path = uppercase_suf_path
+    img = imageio.imread(path)
+    return img
+
+
+def ensure_not_inverted(lab: np.ndarray, threshold: float = 0.5, verbose=True) -> np.ndarray:
+    """Heuristic to ensure that the label is not inverted.
+    
+    It is not plausible that there is more foreground than background in a label image."""
+    if np.any(lab < 0) or np.any(lab > 1):
+        raise ValueError('Labels must be in the range [0, 1] (binary).')
+    if lab.mean().item() > 0.5:
+        if verbose:
+            print('ensure_not_inverted: re-inverting labels')
+        lab = ~lab
+    return lab
+
+
+@dataclass
+class ImageResources:
+    """Image resources, holds all necessary data and metadata of one source image including labels"""
+    metarow: pd.Series
+    raw: Optional[np.ndarray] = None
+    label: Optional[np.ndarray] = None
+    roimask: Optional[np.ndarray] = None
+    rawpath: Optional[Path] = None
+    curated: bool = False
+
+
+def get_image_resources(img_num, sheet_path=None, use_curated_if_available=True):
+    metarow = get_meta_row(path_or_num=img_num, sheet_path=sheet_path)
+    raw_path = get_raw_path(img_num=img_num, sheet_path=sheet_path)
+    if raw_path.is_file():
+        raw = imageio.imread(raw_path)
+    else:
+        raw = None
+    # with_stem() requires py39 -> use with_name()
+    label_path = raw_path.with_name(f'{raw_path.stem}_{LABEL_NAME}{raw_path.suffix}')
+    # Look for improved labels in "curated" subdir. If nothing is found, use regular label file.
+    is_curated = False
+    if use_curated_if_available:
+        for curated_dir_candidate in ['Curated', 'curated']:  # Sometimes lowercase
+            candidate = label_path.parent / curated_dir_candidate / label_path.name
+            if candidate.is_file():
+                label_path = candidate  # Update label_path
+                is_curated = True
+                break  # Found it, stop searching
+    if label_path.is_file():
+        label = imageio.imread(label_path)
+        label = label > 0  # Binarize
+        label = ensure_not_inverted(label)
+    else:
+        label = None
+
+    roimask = None  # TODO. Not implemented yet 
+
+    imgres = ImageResources(
+        raw=raw,
+        label=label,
+        roimask=roimask,
+        metarow=metarow,
+        rawpath=raw_path,
+        curated=is_curated,
+    )
+
+    return imgres
+
