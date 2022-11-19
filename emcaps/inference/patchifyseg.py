a58a37e6f506ef1103d49b5b0ae5b52aeab29925
mdraw <martin.drawitsch@gmail.com>
2022-11-19 20:25:54 +0100

    patchifyseg: Eliminate 1-element loop
    Dedenting a lot of code. No changes in behavior.

--- emcaps/inference/patchifyseg.py
+++ emcaps/inference/patchifyseg.py
@@ -124,7 +124,7 @@ def main(cfg: DictConfig) -> None:
         img_paths.append(img_path)
 
     patch_out_path: str = os.path.expanduser(cfg.patchifyseg.patch_out_path)
-    model_path = [Path(cfg.patchifyseg.model_path)]
+    model_path = Path(cfg.patchifyseg.model_path)
 
     # Create output directories
     for p in [patch_out_path, f'{patch_out_path}/raw', f'{patch_out_path}/mask', f'{patch_out_path}/samples', f'{patch_out_path}/nobg', f'{patch_out_path}/cavg', f'{patch_out_path}/cmax']:
@@ -156,158 +156,157 @@ def main(cfg: DictConfig) -> None:
 
     patch_id = 0  # Incremented below for each patch written to disk
 
-    for model_path in model_paths:
-        predictor = Predictor(
-            model=model_path,
-            device=None,
-            float16=True,
-            transform=pre_predict_transform,
-            augmentations=cfg.patchifyseg.tta_num,
-            apply_softmax=True,
-        )
+    predictor = Predictor(
+        model=str(model_path),
+        device=None,
+        float16=True,
+        transform=pre_predict_transform,
+        augmentations=cfg.patchifyseg.tta_num,
+        apply_softmax=True,
+    )
 
-        for img_path in tqdm.tqdm(img_paths, position=0, desc='Images', dynamic_ncols=True):
-            logger.debug(str(img_path))
+    for img_path in tqdm.tqdm(img_paths, position=0, desc='Images', dynamic_ncols=True):
+        logger.debug(str(img_path))
 
-            imgmeta = utils.get_meta_row(img_path, sheet_path=sheet_path)
-            enctype = imgmeta.scondv5
-            if pd.isna(enctype) or enctype not in DATA_SELECTION:
-                logger.debug(f'enctype {enctype} skipped')
+        imgmeta = utils.get_meta_row(img_path, sheet_path=sheet_path)
+        enctype = imgmeta.scondv5
+        if pd.isna(enctype) or enctype not in DATA_SELECTION:
+            logger.debug(f'enctype {enctype} skipped')
+            continue
+        # if DRO_MODE:  # TODO: use different, more precise flag
+        # enctype = enctype.replace('DRO-', '')  # drop DRO because we want to treat DRO the same as HEK here  #DRO
+        # enctype = enctype.replace('MICE_', '')  # do the same for MICE_
+        enctype = utils.strip_host_prefix(enctype)
+
+        inp = np.array(iio.imread(img_path), dtype=np.float32)[None][None]  # (N=1, C=1, H, W)
+        raw = inp[0][0]
+        if USE_GT:
+            label_path = img_path.with_name(f'{img_path.stem}_{cfg.label_name}.png')
+            label = iio.imread(label_path).astype(np.int64)
+            mask = label
+        else:
+            out = predictor.predict(inp)
+            out = out.numpy()
+
+            assert out.shape[1] == 2
+            cout = out[0, 1]
+            cout = (cout * 255.).astype(np.uint8)
+            mask = cout > thresh
+
+        mask = ndimage.binary_fill_holes(mask).astype(mask.dtype)
+
+        img_num = imgmeta.num
+
+        if ALL_VALIDATION:
+            is_validation = True
+        else:
+            is_validation = '_val' in img_path.stem
+        role = 'val' if is_validation else 'trn'
+        is_train = not is_validation
+
+        cc, n_comps = ndimage.label(mask)
+
+        rprops = measure.regionprops(cc, raw)
+
+        for rp in tqdm.tqdm(rprops, position=1, leave=False, desc='Patches'):
+            centroid = np.round(rp.centroid).astype(np.int64)  # Note: This centroid is in the global coordinate frame
+            if rp.area < EC_MIN_AREA or rp.area > EC_MAX_AREA:
+                logger.info(f'Skipping: area size {rp.area} not within [{EC_MIN_AREA}, {EC_MAX_AREA}]')
+                continue  # Too small or too big (-> background component?) to be a normal particle
+            circularity = np.nan
+            if MIN_CIRCULARITY > 0:
+                circularity = iu.calculate_circularity(rp.perimeter, rp.area)
+                if circularity < MIN_CIRCULARITY:
+                    logger.info(f'Skipping: circularity {circularity} below {MIN_CIRCULARITY}')
+                    continue  # Not circular enough (probably a false merger)
+                circularity = np.round(circularity, 2)  # Round for more readable logging
+
+            lo = centroid - EC_REGION_RADIUS
+            hi = centroid + EC_REGION_RADIUS + EC_REGION_ODD_PLUS1
+            if np.any(lo < 0) or np.any(hi > raw.shape):
+                logger.info(f'Skipping: region touches border')
+                continue  # Too close to image border
+
+            xslice = slice(lo[0], hi[0])
+            yslice = slice(lo[1], hi[1])
+
+            # Get enctype for specific position (for supporting multi-class images)
+            enctype = utils.get_isplit_enctype(path=img_path, pos=tuple(centroid), isplitdata_root=isplitdata_root, role=role)
+            # TODO: Test below condition
+
+            if enctype == '?':
+                logger.info(f'Skipping patch, can\'t determine local enctype: image {img_num=}, {role=}, pos={centroid}')
                 continue
-            # if DRO_MODE:  # TODO: use different, more precise flag
-            # enctype = enctype.replace('DRO-', '')  # drop DRO because we want to treat DRO the same as HEK here  #DRO
-            # enctype = enctype.replace('MICE_', '')  # do the same for MICE_
-            enctype = utils.strip_host_prefix(enctype)
 
-            inp = np.array(iio.imread(img_path), dtype=np.float32)[None][None]  # (N=1, C=1, H, W)
-            raw = inp[0][0]
-            if USE_GT:
-                label_path = img_path.with_name(f'{img_path.stem}_{cfg.label_name}.png')
-                label = iio.imread(label_path).astype(np.int64)
-                mask = label
-            else:
-                out = predictor.predict(inp)
-                out = out.numpy()
+            raw_patch = raw[xslice, yslice]
+            # mask_patch = mask[xslice, yslice]
+            # For some reason mask[xslice, yslice] does not always contain nonzero values, but cc at the same slice does.
+            # So we rebuild the mask at the region slice by comparing cc to 0
+            mask_patch = cc[xslice, yslice] > 0
 
-                assert out.shape[1] == 2
-                cout = out[0, 1]
-                cout = (cout * 255.).astype(np.uint8)
-                mask = cout > thresh
+            # Eliminate coinciding masks from other particles that can overlap with this region (this can happen because we slice the mask_patch from the global mask)
+            _mask_patch_cc, _ = ndimage.label(mask_patch)
+            # Assuming convex particles, the center pixel is always on the actual mask region of interest.
+            _local_center = np.round(np.array(mask_patch.shape) / 2).astype(np.int64)
+            _mask_patch_centroid_label = _mask_patch_cc[tuple(_local_center)]
+            # All mask_patch pixels that don't share the same cc label as the centroid pixel are set to 0
+            mask_patch[_mask_patch_cc != _mask_patch_centroid_label] = 0
 
-            mask = ndimage.binary_fill_holes(mask).astype(mask.dtype)
+            if mask_patch.sum() == 0:
+                # No positive pixel in mask -> skip this one
+                logger.info(f'Skipping: no particle mask in region')
+                # TODO: Why does this happen although we're iterating over regionprops from mask?
+                # (Only happens if using `mask_patch = mask[xslice, yslice]`. Workaround: `Use mask_patch = cc[xslice, yslice] > 0`)
+                continue
 
-            img_num = imgmeta.num
+            area = int(np.sum(mask_patch))
+            radius2 = np.round(measure_outer_disk_radius(mask_patch, discrete=False), 1)
 
-            if ALL_VALIDATION:
-                is_validation = True
-            else:
-                is_validation = '_val' in img_path.stem
-            role = 'val' if is_validation else 'trn'
-            is_train = not is_validation
+            # Enlarge masks because we don't want to risk losing perimeter regions
+            if DILATE_MASKS_BY > 0:
+                disk = sm.disk(DILATE_MASKS_BY)
+                # mask_patch = ndimage.binary_dilation(mask_patch, iterations=DILATE_MASKS_BY)
+                mask_patch = sm.binary_dilation(mask_patch, footprint=disk)
 
-            cc, n_comps = ndimage.label(mask)
+            # Measure again after mask dilation
+            area_dilated = int(np.sum(mask_patch))
+            radius2_dilated = np.round(measure_outer_disk_radius(mask_patch, discrete=False), 1)
 
-            rprops = measure.regionprops(cc, raw)
+            # Raw patch with background erased via mask
+            nobg_patch = raw_patch.copy()
+            nobg_patch[mask_patch == 0] = 0
 
-            for rp in tqdm.tqdm(rprops, position=1, leave=False, desc='Patches'):
-                centroid = np.round(rp.centroid).astype(np.int64)  # Note: This centroid is in the global coordinate frame
-                if rp.area < EC_MIN_AREA or rp.area > EC_MAX_AREA:
-                    logger.info(f'Skipping: area size {rp.area} not within [{EC_MIN_AREA}, {EC_MAX_AREA}]')
-                    continue  # Too small or too big (-> background component?) to be a normal particle
-                circularity = np.nan
-                if MIN_CIRCULARITY > 0:
-                    circularity = iu.calculate_circularity(rp.perimeter, rp.area)
-                    if circularity < MIN_CIRCULARITY:
-                        logger.info(f'Skipping: circularity {circularity} below {MIN_CIRCULARITY}')
-                        continue  # Not circular enough (probably a false merger)
-                    circularity = np.round(circularity, 2)  # Round for more readable logging
+            # Concentric average image
+            cavg_patch = concentric_average(raw_patch)
 
-                lo = centroid - EC_REGION_RADIUS
-                hi = centroid + EC_REGION_RADIUS + EC_REGION_ODD_PLUS1
-                if np.any(lo < 0) or np.any(hi > raw.shape):
-                    logger.info(f'Skipping: region touches border')
-                    continue  # Too close to image border
+            raw_patch_fname = f'{patch_out_path}/raw/raw_patch_{patch_id:06d}.png'
+            mask_patch_fname = f'{patch_out_path}/mask/mask_patch_{patch_id:06d}.png'
+            nobg_patch_fname = f'{patch_out_path}/nobg/nobg_patch_{patch_id:06d}.png'
+            cavg_patch_fname = f'{patch_out_path}/cavg/cavg_patch_{patch_id:06d}.png'
 
-                xslice = slice(lo[0], hi[0])
-                yslice = slice(lo[1], hi[1])
+            patchmeta.append(PatchMeta(
+                # patch_id=patch_id,
+                patch_fname=os.path.basename(raw_patch_fname),
+                img_num=img_num,
+                enctype=enctype,
+                centroid_y=centroid[0],
+                centroid_x=centroid[1],
+                corner_y=lo[0],
+                corner_x=lo[1],
+                train=is_train,
+                validation=is_validation,
+                radius2=radius2,
+                radius2_dilated=radius2_dilated,
+                area=area,
+                area_dilated=area_dilated,
+                circularity=circularity,
+            ))
 
-                # Get enctype for specific position (for supporting multi-class images)
-                enctype = utils.get_isplit_enctype(path=img_path, pos=tuple(centroid), isplitdata_root=isplitdata_root, role=role)
-                # TODO: Test below condition
-
-                if enctype == '?':
-                    logger.info(f'Skipping patch, can\'t determine local enctype: image {img_num=}, {role=}, pos={centroid}')
-                    continue
-
-                raw_patch = raw[xslice, yslice]
-                # mask_patch = mask[xslice, yslice]
-                # For some reason mask[xslice, yslice] does not always contain nonzero values, but cc at the same slice does.
-                # So we rebuild the mask at the region slice by comparing cc to 0
-                mask_patch = cc[xslice, yslice] > 0
-
-                # Eliminate coinciding masks from other particles that can overlap with this region (this can happen because we slice the mask_patch from the global mask)
-                _mask_patch_cc, _ = ndimage.label(mask_patch)
-                # Assuming convex particles, the center pixel is always on the actual mask region of interest.
-                _local_center = np.round(np.array(mask_patch.shape) / 2).astype(np.int64)
-                _mask_patch_centroid_label = _mask_patch_cc[tuple(_local_center)]
-                # All mask_patch pixels that don't share the same cc label as the centroid pixel are set to 0
-                mask_patch[_mask_patch_cc != _mask_patch_centroid_label] = 0
-
-                if mask_patch.sum() == 0:
-                    # No positive pixel in mask -> skip this one
-                    logger.info(f'Skipping: no particle mask in region')
-                    # TODO: Why does this happen although we're iterating over regionprops from mask?
-                    # (Only happens if using `mask_patch = mask[xslice, yslice]`. Workaround: `Use mask_patch = cc[xslice, yslice] > 0`)
-                    continue
-
-                area = int(np.sum(mask_patch))
-                radius2 = np.round(measure_outer_disk_radius(mask_patch, discrete=False), 1)
-
-                # Enlarge masks because we don't want to risk losing perimeter regions
-                if DILATE_MASKS_BY > 0:
-                    disk = sm.disk(DILATE_MASKS_BY)
-                    # mask_patch = ndimage.binary_dilation(mask_patch, iterations=DILATE_MASKS_BY)
-                    mask_patch = sm.binary_dilation(mask_patch, footprint=disk)
-
-                # Measure again after mask dilation
-                area_dilated = int(np.sum(mask_patch))
-                radius2_dilated = np.round(measure_outer_disk_radius(mask_patch, discrete=False), 1)
-
-                # Raw patch with background erased via mask
-                nobg_patch = raw_patch.copy()
-                nobg_patch[mask_patch == 0] = 0
-
-                # Concentric average image
-                cavg_patch = concentric_average(raw_patch)
-
-                raw_patch_fname = f'{patch_out_path}/raw/raw_patch_{patch_id:06d}.png'
-                mask_patch_fname = f'{patch_out_path}/mask/mask_patch_{patch_id:06d}.png'
-                nobg_patch_fname = f'{patch_out_path}/nobg/nobg_patch_{patch_id:06d}.png'
-                cavg_patch_fname = f'{patch_out_path}/cavg/cavg_patch_{patch_id:06d}.png'
-
-                patchmeta.append(PatchMeta(
-                    # patch_id=patch_id,
-                    patch_fname=os.path.basename(raw_patch_fname),
-                    img_num=img_num,
-                    enctype=enctype,
-                    centroid_y=centroid[0],
-                    centroid_x=centroid[1],
-                    corner_y=lo[0],
-                    corner_x=lo[1],
-                    train=is_train,
-                    validation=is_validation,
-                    radius2=radius2,
-                    radius2_dilated=radius2_dilated,
-                    area=area,
-                    area_dilated=area_dilated,
-                    circularity=circularity,
-                ))
-
-                iio.imwrite(raw_patch_fname, raw_patch.astype(np.uint8))
-                iio.imwrite(mask_patch_fname, mask_patch.astype(np.uint8) * 255)
-                iio.imwrite(nobg_patch_fname, nobg_patch.astype(np.uint8))
-                iio.imwrite(cavg_patch_fname, cavg_patch.astype(np.uint8))
-                patch_id += 1
+            iio.imwrite(raw_patch_fname, raw_patch.astype(np.uint8))
+            iio.imwrite(mask_patch_fname, mask_patch.astype(np.uint8) * 255)
+            iio.imwrite(nobg_patch_fname, nobg_patch.astype(np.uint8))
+            iio.imwrite(cavg_patch_fname, cavg_patch.astype(np.uint8))
+            patch_id += 1
 
 
     patchmeta = pd.DataFrame(
